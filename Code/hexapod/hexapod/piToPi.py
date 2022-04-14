73b24e0bd633da45f23e33e04aa1c31917359812
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-14 11:50:20 -0400

    feat: Changes the emg polling function output
    The EMG polling function now sends back both the EMG sensor outputs and
    constrains both values to a number from 0 to 1.

--- Code/hexapod/hexapod/piToPi.py
+++ Code/hexapod/hexapod/piToPi.py
@@ -1,7 +1,16 @@
-def pollWalkEMG():
+def pollEMG():
     # TODO: Get the recorded EMG from the raspberry pi zero and normalize it.
-    return 1
+    fcr_emg = 1
+    edc_emg = 1
 
-def pollTurnEMG():
-    # TODO: Get the recorded EMG from the raspberry pi zero and normalize it.
-    return 1
+    if fcr_emg > 1:
+        fcr_emg = 1
+    elif fcr_emg < 0:
+        fcr_emg = 0
+
+    if edc_emg > 1:
+        edc_emg = 1
+    elif edc_emg < 0:
+        edc_emg = 0
+
+    return [fcr_emg, edc_emg]
