99b3dfce4d7488e97fae9daa7209c245f6e953d1
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-15 10:57:05 -0400

    feat: Adds code to record EMG
    Adds the code to record from the ADC to get the forearm EMG values.
    Issue #11 still needs to be addressed to send this data wirelessly to
    the other raspberry pi, but this script will record the EMG. When the
    wireless communication is made, the pollEMG function will need to
    incorporate it somehow.
    
    Closes #12 issue

--- Code/hexapod/hexapod/piToPi.py
+++ Code/hexapod/hexapod/piToPi.py
@@ -1,12 +1,25 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
 def pollEMG():
-    # TODO: Get the recorded EMG from the raspberry pi zero and normalize it.
-    fcr_emg = 1
-    edc_emg = 1
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    cs = digitalio.DigitalInOut(board.D5)
+    mcp = MCP.MCP3008(spi, cs)
 
-    fcr_emg = min(fcr_emg, 1)
-    fcr_emg = max(fcr_emg, 0)
+    #setup the EMG channels to record from
+    fcr_channel = AnalogIn(mcp, MCP.P0)
+    edc_channel = AnalogIn(mcp, MCP.P1)
+    #get the 16 value on each EMG channel and normalize it
+    fcr_emg = fcr_channel.value / 65536.0
+    edc_emg = edc_channel.value / 65536.0
 
-    edc_emg = min(edc_emg, 1)
-    edc_emg = max(edc_emg, 0)
+    fcr_emg = min(fcr_emg, 1.0)
+    fcr_emg = max(fcr_emg, 0.0)
+
+    edc_emg = min(edc_emg, 1.0)
+    edc_emg = max(edc_emg, 0.0)
 
     return [fcr_emg, edc_emg]
