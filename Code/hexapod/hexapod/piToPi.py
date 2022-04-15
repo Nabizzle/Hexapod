4d8ec57d960c24096a25fbc4a1797b2f874036fb
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-14 23:05:49 -0400

    perf: Uses max and min functions to constrain EMG

--- Code/hexapod/hexapod/piToPi.py
+++ Code/hexapod/hexapod/piToPi.py
@@ -3,14 +3,10 @@ def pollEMG():
     fcr_emg = 1
     edc_emg = 1
 
-    if fcr_emg > 1:
-        fcr_emg = 1
-    elif fcr_emg < 0:
-        fcr_emg = 0
+    fcr_emg = min(fcr_emg, 1)
+    fcr_emg = max(fcr_emg, 0)
 
-    if edc_emg > 1:
-        edc_emg = 1
-    elif edc_emg < 0:
-        edc_emg = 0
+    edc_emg = min(edc_emg, 1)
+    edc_emg = max(edc_emg, 0)
 
     return [fcr_emg, edc_emg]
