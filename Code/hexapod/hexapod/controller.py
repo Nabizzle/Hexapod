da89af3859ba01011d780a9f245d1f5d41c812d3
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-20 14:52:41 -0400

    docs: Fleshes out doc strings
    Adds more descriptive doc strings to the module

--- Code/hexapod/hexapod/controller.py
+++ Code/hexapod/hexapod/controller.py
@@ -1,3 +1,19 @@
+"""
+Scripts for controlling the hexapod.
+
+These are scripts that control how the hexapod walks and turns. This module
+combines the work of the rest of the library and as a result, imports most of
+it to utilize most of the functions.
+
+Functions
+---------
+controller: Controls the hexapod to walk or turn based on EMG.
+sit: Tells the Hexapod to sit with its body on the ground.
+stand: Tells the hexapod to stand in the neutral position.
+walkCycle:
+    Tells the hexapod to walk a specified distance without the need for EMG
+sendPositions: Send each position in a set to the servo controller.
+"""
 from hexapod.leg import recalculateLegAngles, startLegPos, legModel
 from hexapod.body import bodyPos
 from hexapod.move import (switchMode, emgToWalk, resetWalkStance, emgToTurn,
@@ -9,7 +25,32 @@ from time import sleep
 
 
 def controller(mode: bool) -> None:
-    """controls the hexapod to walk or turn and send the commands"""
+    """
+    Controls the hexapod to walk or turn based on EMG.
+
+    Takes in EMG signals and tells the hexapod how to walk or turn based on
+    those EMG signals. The signals are sent over a COM port to the Lynxmotion
+    SSC32U servo controller. This controller combines the work of the rest of
+    the library to get the hexapod to move. This is the main function to run
+    when using the hexapod for its intended purpose.
+
+    Parameters
+    ----------
+    mode: bool
+        This parameter determines if the hexapod is walking or turning. `mode`
+        equalling 1 is walking and 0 is turning.
+
+    Notes
+    -----
+    This function has the serial port to communicate to and the threshold
+    of cocontraction to switch modes hardcoded.
+
+    See Also
+    --------
+    move.switchMode:
+        Sends a boolean based on EMG to indicate when to change movement
+        modes.
+    """
     port = connect('COM1')  # connect to the servo controller
     # setup the starting robot positions
     body_model = bodyPos(pitch=0, roll=0, yaw=0, Tx=0, Ty=0, Tz=0,
@@ -51,7 +92,18 @@ def controller(mode: bool) -> None:
 
 
 def sit(port: Any) -> None:
-    """tells the Hexapod to sit with its body on the ground"""
+    """
+    Tells the Hexapod to sit with its body on the ground.
+    
+    Recreates the neutral hexapod positions, but with a lowered height so that
+    the body of the hexapod touches the ground. This is the position that the
+    hexapod should be turned off in.
+
+    Parameters
+    ----------
+    port: Serial Port
+        The COM port that the servo signals are sent over.
+    """
     body_model = bodyPos(pitch=0, roll=0, yaw=0, Tx=0, Ty=0, Tz=0,
                          body_offset=85)
     sit_leg = startLegPos(body_model, start_radius=90, start_height=10)
@@ -61,7 +113,22 @@ def sit(port: Any) -> None:
 
 
 def stand() -> None:
-    """tells the hexapod to stand for the first time"""
+    """
+    Tells the hexapod to stand in the neutral position.
+    
+    Recreates the neutral hexapod positions that occur at the beginning
+    of the controller function.
+
+    Notes
+    -----
+    This function assumes that there is already a connection to the serial
+    port for the Lynxmotion SSC32U.
+
+    See Also
+    --------
+    controller:
+        Controls the hexapod to walk or turn based on EMG.
+    """
     # controls the hexapod to walk or turn and send the commands
     port = connect('COM4')  # connect to the servo controller
     # setup the starting robot positions
@@ -75,15 +142,65 @@ def stand() -> None:
 
 def walkCycle(port: Any, body_model: NDArray, leg_model: NDArray,
               distance: float, angle: float) -> None:
-    """Tells the hexapod to walk a specified distance without the need for EMG"""
+    """
+    Tells the hexapod to walk a specified distance without the need for EMG.
+    
+    This function just uses the move.walk function and sends its positions to
+    the Lynxmotion SSC32U.
+
+    Parameters
+    ----------
+    port: Serial Port
+        The COM port that the servo signals are sent over.
+    body_model: NDArray
+        The 7x3 numpy array containing the locations of the coax servos.
+    leg_model: NDArray
+        the 4x3x6 numpy array that holds the locations of the coax, femur,
+        and tibia servos as well as the feet end positions.
+    distance: float
+        The length in millimeters that the hexapod will walk. This distance is
+        broken up into steps based on the max step size in move.walk
+    angle: float
+        The direction the hexapod will walk in. 90 degrees is forward.
+
+    See Also
+    --------
+    move.walk
+    """
     positions = walk(leg_model, distance, angle)
     sendPositions(port, positions, body_model)
 
 
 def sendPositions(port: Any, positions: NDArray, body_model: NDArray) -> bool:
     """
-    Send each position in a set of hexapod command positions to the servo
-    controller
+    Send each position in a set to the servo controller.
+
+    Takes a list of command positions and iterates through them to send each
+    position as a servo command to the Lynxmotion SSC32U.
+
+    Parameters
+    ----------
+    port: Serial Port
+        The COM port that the servo signals are sent over
+    positions: NDArray
+        A numpy array of a set of foot positions to command the hexapod to.
+        These foot positions are a 6x3 numpy array of x, y, z positions for
+        the six legs and the number of 6x3 arrays in the positions matrix is
+        determined by how far the hexapod walked or turned.
+    body_model: NDArray
+        The 7x3 numpy array containing the locations of the coax servos.
+
+    Returns
+    -------
+    bool:
+        The function returns true when finished.
+
+    Notes
+    -----
+    In this function, the ssc32uDriver.anglesToSerial function does not have
+    its speed or time inputs used as each change in position is so small, that
+    using these parameters is unneeded. Also note that there is a 5ms delay
+    between commands.
     """
     for position in positions:
         # convert the feet positions to angles
