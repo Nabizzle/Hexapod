853d0dbe6502b8c13f6ae56eead4e040c85049ed
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-22 21:59:33 -0400

    fix: Addresses style issues.

--- Code/hexapod/docs/conf.py
+++ Code/hexapod/docs/conf.py
@@ -58,4 +58,7 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+html_sidebars = {
+    '**': ['globaltoc.html', 'relations.html',
+    'sourcelink.html', 'searchbox.html']
+    }
