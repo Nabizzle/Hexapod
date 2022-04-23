4ed087d11fb14da5086c9358d0ed0df4f7160c7c
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-22 21:09:03 -0400

    fix: Improves sit function

--- Code/hexapod/hexapod/controller.py
+++ Code/hexapod/hexapod/controller.py
@@ -110,7 +110,7 @@ def sit(port: Any) -> None:
     """
     body_model = bodyPos(pitch=0, roll=0, yaw=0, Tx=0, Ty=0, Tz=0,
                          body_offset=85)
-    sit_leg = startLegPos(body_model, start_radius=90, start_height=10)
+    sit_leg = startLegPos(body_model, start_radius=120, start_height=10)
     # get the serial message from the angles
     message = anglesToSerial(sit_leg, 500, 2000)
     sendData(port, message)  # send the serial message
