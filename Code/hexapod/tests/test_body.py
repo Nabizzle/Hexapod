5858951542efc43b801e494c41f03a11360fd711
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2022-04-28 11:04:43 -0400

    refactor: visually aligned arrays.

--- Code/hexapod/tests/test_body.py
+++ Code/hexapod/tests/test_body.py
@@ -2,11 +2,12 @@ from hexapod import body
 import numpy as np
 
 def test_bodyPos():
-    """Tests the default body positions is correct"""
-    assert np.allclose(body.bodyPos(0, 0, 0, 0, 0, 0, 85), np.array([[42.5, 73.61215932, 0],
-                                                                    [85, 0, 0],
-                                                                    [42.5, -73.61215932, 0],
-                                                                    [-42.5, -73.61215932, 0],
-                                                                    [-85, 0, 0],
-                                                                    [-42.5, 73.61215932, 0],
-                                                                    [42.5, 73.61215932, 0]]))
+    """Tests the default body positions are correct."""
+    assert np.allclose(body.bodyPos(0, 0, 0, 0, 0, 0, 85),
+                       np.array([[42.5, 73.61215932, 0],
+                                 [85, 0, 0],
+                                 [42.5, -73.61215932, 0],
+                                 [-42.5, -73.61215932, 0],
+                                 [-85, 0, 0],
+                                 [-42.5, 73.61215932, 0],
+                                 [42.5, 73.61215932, 0]]))
--- Code/hexapod/tests/test_ssc32uDriver.py
+++ Code/hexapod/tests/test_ssc32uDriver.py
@@ -13,11 +13,11 @@ def test_angleToPW():
 def test_anglesToSerial():
     """Tests that the message to the servo controller is made and formatted correctly and as a btye string"""
     angles = np.array([[60, 60, 60],
-                        [0, 60, 60],
-                        [-60, 60, 60],
-                        [-120, 60, 60],
-                        [180, 60, 60],
-                        [120, 60, 60]])
+                       [0, 60, 60],
+                       [-60, 60, 60],
+                       [-120, 60, 60],
+                       [180, 60, 60],
+                       [120, 60, 60]])
     speed = 500
     time = 1000
     assert ssc32uDriver.anglesToSerial(angles, speed, time) == bytes('#0P833S500' + '#1P833S500' + '#2P2167S500' + '#3P1500S500' + '#4P833S500' + '#5P2167S500' + '#6P2167S500' + '#7P833S500' + '#8P2167S500' + '#9P833S500' + '#10P2167S500' + '#11P833S500' + '#12P1500S500' + '#13P2167S500' + '#14P833S500' + '#15P2167S500' + '#16P2167S500' + '#17P833S500' + 'T1000\r', 'ascii')
@@ -26,10 +26,10 @@ def test_anglesToSerial():
 def test_anglesToSerialWrongFormat():
     """Tests that an incorrectly formatted message raises an exception"""
     angles = np.array([[60, 60, 60],
-                        [0, 60, 60],
-                        [-60, 60, 60],
-                        [-120, 60, 60],
-                        [180, 60, 60]])
+                       [0, 60, 60],
+                       [-60, 60, 60],
+                       [-120, 60, 60],
+                       [180, 60, 60]])
     speed = 500
     time = 1000
     with pytest.raises(Exception):
